bac793696273aee327fe603ede190fa28368a177
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2019-01-04 08:56:47 -0200

    Pois dessa forma não está funcionando o modal

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,12 +3,12 @@ function editar(id){
 		url:BASE_URL+"usuarios/editar/" + id,
 		type:'POST',
 		data:{id:id},
-/*		beforeSend:function(){
+		beforeSend:function(){
 			//Antes de enviarmos o html vamos fazer algo antes
 			$('#modal').find('.modal-body').html('Carregando ...');
 			$('#modal').modal('show');
 		}
-*/		success:function(html){
+		success:function(html){
 			//Pegando o html para colocar dentro do modal
 			$('#modal').find('.modal-body').html(html);
 			$('#modal').modal('show');
